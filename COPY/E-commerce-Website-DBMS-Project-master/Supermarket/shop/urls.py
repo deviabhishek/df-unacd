a90e00706736fccf1366de4063f6d92eea086cde
deviabhishek <deviabhishek.devi.com>
2023-03-16 07:21:20 +0530

    trail

--- COPY/E-commerce-Website-DBMS-Project-master/Supermarket/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-path("", views.index, name="shophome"),
-path("products/<int:id>",views.productView, name="ProductView"),
-path("checkout",views.checkout, name="checkout")
-]
